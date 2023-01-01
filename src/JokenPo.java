8c3a54e0729f208746c2db1f387b7dd0dc87a3e4
felipeperaltacheung <f33___@hotmail.com>
2023-01-01 12:39:06 -0300

    Condicao para verificar se escolheu de 1 a 3

--- src/JokenPo.java
+++ src/JokenPo.java
@@ -1,14 +1,31 @@
+import javax.security.sasl.SaslClient;
+import java.util.Scanner;
+
 public record JokenPo(
         Player user,
         Player IA,
         int rounds
 ) {
 
-     public void toPlay(){
+     public void toPlay() {
           System.out.println("\n******* Welcome, " + user.getName() + " *******\n");
+          int choiceUser = choiceUser();
+          if(choiceUser < 1 || choiceUser >3){
+               System.out.println("\n INVALID MOVE! (1, 2 or 3): ");
+               System.out.println("\tPOINT TO " + IA.getName() + "\n");
+          }
+
+
      }
+     private int choiceUser(){ //escolha do usuario
+          Scanner sc = new Scanner(System.in);
 
-     private int choiceUser(){
+          System.out.println("1 - PEDRA ");
+          System.out.println("2 - PAPEL ");
+          System.out.println("3 - TESOURA ");
 
+          System.out.print("Informe sua jogada: ");
+          
+          return sc.nextInt();
      }
 }
