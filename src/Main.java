8968ee0a853c4102ef2eed23147f59b951721e26
felipeperaltacheung <f33___@hotmail.com>
2023-01-01 12:08:40 -0300

    jogadores

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static JokenPo startGame(){
+        System.out.println("******* Jo-Ken-Po  *******\n"); // \n -> pular linha
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("type you name");
+        String playerName = sc.next().toUpperCase();
+
+        Player user = new Player(playerName); // instanciado o objeto capturado do sc.next
+
+        Player IA = new Player(IA);
+
+    }
+
     public static void main(String[] args) {
         System.out.println("A");
     }
