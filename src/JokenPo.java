eb9c9b142c8966d94e6d3816555da91809d53c13
felipeperaltacheung <f33___@hotmail.com>
2023-01-09 01:04:58 -0300

    final

--- src/JokenPo.java
+++ src/JokenPo.java
@@ -1,4 +1,5 @@
 import javax.security.sasl.SaslClient;
+import java.util.Random;
 import java.util.Scanner;
 
 public record JokenPo(
@@ -10,14 +11,61 @@ public record JokenPo(
      public void toPlay() {
           System.out.println("\n******* Welcome, " + user.getName() + " *******\n");
 
-          int choiceUser = choiceUser();
-          if(choiceUser < 1 || choiceUser >3){
-               System.out.println("\n INVALID MOVE! (1, 2 or 3): ");
-               System.out.println("\tPOINT TO " + IA.getName() + "\n");
-               IA.incrementScore();
+          for (int i = 1;  i < rounds; i++){
+               int choiceUser = choiceUser();
+               if(choiceUser < 1 || choiceUser >3){
+                    System.out.println("\n INVALID MOVE! (1, 2 or 3): ");
+                    System.out.println("\tPOINT TO " + IA.getName() + "\n");
+                    IA.incrementScore();
+                    //continue
+               }
+               int choiceIA = choiceIA();
+               System.out.println("\n" + choiceUser + " x " + choiceIA);
+
+               int result = choiceUser - choiceIA;
+
+               winnerRound(result);
+          }
+     }
+
+
+     public void showFinalResult(){
+          System.out.println("\n**********************************************88\n");
+
+          Integer finalScoreUser = user.getScore();
+          Integer finalScoreIA = IA.getScore();
+
+          System.out.println("\n\tPLACAR FINAL: " + user.getName() + " " + user.getScore() + " X " + IA.getScore() + " " + IA.getName());
+
+          if (finalScoreUser == finalScoreIA) {
+               System.out.println("\t\t\tEMPATE!");
+          } else {
+               String finalWinner = (finalScoreIA > finalScoreUser) ?  user.getName() : IA.getName();
+
+               System.out.println("\t\tVENCEDOR = " + finalWinner.toUpperCase());
           }
 
+          System.out.println("\n**********************************************88\n");
 
+     }
+     private void winnerRound(int result){
+          String winnerRound;
+          if(result == 0){
+               winnerRound = "EMPATE";
+          } else {
+               if (result == -1 || result ==2){
+                    IA.incrementScore();
+                    winnerRound = IA().getName();
+               } else {
+                    user.incrementScore();
+                    winnerRound = user.getName();
+               }
+          }
+     }
+
+     private int choiceIA(){
+          Random r = new Random();
+          return r.nextInt(3) +1;
      }
      private int choiceUser(){ //escolha do usuario
           Scanner sc = new Scanner(System.in);
--- src/Main.java
+++ src/Main.java
@@ -22,5 +22,6 @@ public class Main {
     public static void main(String[] args) {
         JokenPo jokenPo = startGame();
         jokenPo.toPlay();
+        jokenPo.showFinalResult();
     }
 }
